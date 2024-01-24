0cb18a34341ab4b44efb2517196eed8478d657d2
madicooks <cook441@purdue.edu>
2024-01-24 12:23:54 -0500

    in progress lab 12:23pm

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -3,10 +3,78 @@
 
 #include <iostream>
 
-int main()
+using namespace std;
+
+void foo(float A, float B)
 {
-    std::cout << "Hello World!\n";
+    //does something
 }
+
+void printMenu(int& choice) {
+cout << "Welcome to my program, please pick one:" << endl;
+cout << "1. Addition" << endl;
+cout << "2. Subtraction" << endl;
+cout << "3. Multiplication" << endl;
+cout << "4. Division" << endl;
+
+cout << "\nYour Selction: ";
+cin >> choice;
+}
+
+void getChoices(float& A, float& B) {
+    cout << "This is proving the first choice ran" << endl;
+}
+
+void firstChoice(float A, float B) {
+    cout << "Please enter the first value:";
+    cin >> A;
+
+}
+
+void secondChoice(float A, float B) {
+    cout << "This is proving the second choice ran" << endl;
+    
+}
+
+void thirdChoice(float A, float B) {
+    cout << "This is proving the third choice ran" << endl;
+
+}
+
+void fourthChoice(float a, float B) {
+    cout << "This is proving the fourth choice ran" << endl;
+
+}
+
+
+    int main()
+{
+        int choice; 
+        float A; 
+        float B; 
+
+        printMenu(choice); 
+        getChoices(A, B);
+
+        if (choice == 1) {
+            firstChoice(A, B); 
+        }
+
+        if (choice == 2) {
+            secondChoice(A, B);
+        }
+
+        if (choice == 3) {
+            thirdChoice(A, B);
+        }
+
+        if (choice == 4) {
+            fourthChoice(A, B);
+
+        }
+      
+} 
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
